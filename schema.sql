c490eed30a6063d8f7266b307c875832cdcd163f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-10 15:27:36 +0100

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer PRIMARY KEY,
+    name varchar(100),
+    date_of_birth date not null,
+    escape_attempts integer not null,
+    neutered boolean not null,
+    weight_kg decimal 
+    
 );
