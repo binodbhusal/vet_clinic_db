72b4435edba1f2ab1722acb347fb18a4d63eb7b0
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-12 17:54:11 +0100

    insert data

--- data.sql
+++ data.sql
@@ -22,3 +22,30 @@ INSERT INTO animals(id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES(11,'Blossom','1998-10-13',3,true,17) ;
 INSERT INTO animals(id,name,date_of_birth,escape_attempts,neutered,weight_kg) 
 VALUES(12,'Ditto','2022-05-14',4,true,22) ;
+
+--Insert the following data into the owners table:
+insert into owners(full_name,age) values('Sam Smith', 34);
+insert into owners(full_name,age) values('Jenifier Orwell', 19);
+insert into owners(full_name,age) values('Bob', 45);
+insert into owners(full_name,age) values('Melody Pond', 77);
+insert into owners(full_name,age) values('Dean Winchester', 14);
+insert into owners(full_name,age) values('Jodie Whittaker', 38);
+
+--Insert the following data into the species table:
+insert into species (name)values('Pokemon');
+insert into species (name)values('Digimon');
+
+--Modify your inserted animals so it includes the species_id value:
+
+UPDATE animals set species_id =(select id from species where name='Digimon')where name like '%mon';
+UPDATE animals set species_id =(select id from species where name='Pokemon')where species_id is null;
+
+--Modify your inserted animals to include owner information (owner_id):
+update animals set owner_id=(select id from owners where full_name='Sam Smith') where name
+IN ('Agumon');
+update animals set owner_id=(select id from owners where full_name='Jennifer Orwell') where name
+IN ('Gabumon', 'Pikachu');
+update animals set owner_id=(select id from owners where full_name='Bob') where name IN('Devimon','Plantmon');
+update animals set owner_id=(select id from owners where full_name='Melody Pond') where name IN('Charmander','Squirtle','Blossom');
+update animals set owner_id=(select id from owners where full_name='Dean Winchester') where name
+IN('Angemon','Boarmon');
